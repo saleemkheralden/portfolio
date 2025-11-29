51c359f25e8519a13a9be2014660bf11e80c8a8f
saleemkheralden <saleemkheralden@gmail.com>
2025-11-29 02:08:51 +0200

    portfolio updates

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,13 @@ export default function About() {
     <section id="about" className="mb-16">
       <h3 className="section-title mb-4">About Me</h3>
       <p className="section-text">
-        I am an **AI Engineer** at the IDF, holding a B.Sc. in Data Science from
-        the Technion. I specialize in the full lifecycle of AI systems, from
-        building robust data pipelines (ETL/MLOps) to developing and deploying
-        cutting-edge machine learning and deep learning models. My background in
-        Data Engineering and Algorithms allows me to transform complex, raw data
-        into scalable, high-impact intelligent solutions.
+        I am an <span className="font-bold">AI Engineer</span> at the IDF,
+        holding a B.Sc. in Data Science from the Technion. I specialize in the
+        full lifecycle of AI systems, from building robust data pipelines
+        (ETL/MLOps) to developing and deploying cutting-edge machine learning
+        and deep learning models. My background in Data Engineering and
+        Algorithms allows me to transform complex, raw data into scalable,
+        high-impact intelligent solutions.
       </p>
     </section>
   );
